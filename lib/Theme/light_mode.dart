767b5fb4b3108a4a2efab84309cf0653e8b81763
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-02-03 01:00:23 +0530

    Second commit

---
+++ lib/Theme/light_mode.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightmode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background:Colors.grey.shade200,
+    primary:Colors.lightBlue.shade800,
+    secondary: Colors.blueAccent,
+  ),
+);
